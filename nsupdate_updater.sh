a41476f26bedf6dd351116d1729c84d956aa615c
Patzgge <48275655+Patzgge@users.noreply.github.com>
2019-05-06 13:52:46 +0200

    Update nsupdate_updater.sh

--- nsupdate_updater.sh
+++ nsupdate_updater.sh
@@ -10,7 +10,7 @@ lastipv4=$(cat /etc/lastip.txt | egrep -i -o $ipv4regex)
 lastipv6=$(cat /etc/lastip.txt | egrep -i -o $ipv6regex)
 
 updateurlbaseipv4="https://NSUPDATEHOSTNAME.nsupdate.info:NSUPDATESECRET@ipv4.nsupdate.info/nic/update?myip="
-updateurlbaseipv6="https://NSUPDATEHOSTNAME.nsupdate.info:NSUPDATESECRET@ipv4.nsupdate.info/nic/update?myip="
+updateurlbaseipv6="https://NSUPDATEHOSTNAME.nsupdate.info:NSUPDATESECRET@ipv6.nsupdate.info/nic/update?myip="
 
 currentipv4=$(ip -f inet addr show $interface | egrep -i -o "inet.*peer" | egrep -i -o $ipv4regex)
 currentipv6=$(ip -f inet6 addr show $interface | egrep -i -o "inet6.*scope global dynamic" | egrep -i -o $ipv6regex)
