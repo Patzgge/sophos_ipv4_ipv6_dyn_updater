c2f5bf9415eafb4429ce433ac7af141f8b96d9bf
Patzgge <48275655+Patzgge@users.noreply.github.com>
2019-05-11 14:00:31 +0200

    Update nsupdate_updater.sh

--- nsupdate_updater.sh
+++ nsupdate_updater.sh
@@ -1,36 +1,69 @@
 #!/bin/sh
 #NSUpdate.info updater script
 
+#Interface variable
 interface=ppp0
-ipv4regex="\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"
-ipv6regex="([0-9]|[a-f]|:){4,}"
+
+#Variable to save the last IPs in File
 lastipfile=/etc/lastip.txt
 
+#NSUpdate Hostename and API Key
+hostname=NSUPDATEHOSTNAME
+apitoken=NSUPDATEAPIKEY
+
+#Public DNS Server variable
+pubdnssrvipv4=208.67.222.222
+pubdnssrvipv6=2620:119:35::35
+
+#Regex to find IPv4 & IPv6 Adress in String
+ipv4regex="\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"
+ipv6regex="([0-9]|[a-f]|:){4,}"
+
+#Find IPs in dte LastIP File
 lastipv4=$(cat /etc/lastip.txt | egrep -i -o $ipv4regex)
 lastipv6=$(cat /etc/lastip.txt | egrep -i -o $ipv6regex)
 
-updateurlbaseipv4="https://NSUPDATEHOSTNAME.nsupdate.info:NSUPDATESECRET@ipv4.nsupdate.info/nic/update?myip="
-updateurlbaseipv6="https://NSUPDATEHOSTNAME.nsupdate.info:NSUPDATESECRET@ipv6.nsupdate.info/nic/update?myip="
-
+#Find current IPs in Interface
 currentipv4=$(ip -f inet addr show $interface | egrep -i -o "inet.*peer" | egrep -i -o $ipv4regex)
 currentipv6=$(ip -f inet6 addr show $interface | egrep -i -o "inet6.*scope global dynamic" | egrep -i -o $ipv6regex)
 
-updateurlipv4=$updateurlbaseipv4$currentipv4
-updateurlipv6=$updateurlbaseipv6$currentipv6
+#Find IPs for Hostname online
+dnsipv4=$(nslookup -query=A $hostname $pubdnssrvipv4 | egrep -i -o "Address.*" | egrep -i -o $ipv4regex | egrep -v $pubdnssrvipv4)
+dnsipv6=$(nslookup -query=AAAA $hostname $pubdnssrvipv6 | egrep -i -o "address.*" | egrep -i -o $ipv6regex | egrep -v $pubdnssrvipv6)
+
+#Base Update URL for NSUpdate Service
+updateurlbaseipv4="@ipv4.nsupdate.info/nic/update?myip="
+updateurlbaseipv6="@ipv4.nsupdate.info/nic/update?myip="
+
+#Create update URL for NSUpdate Service over HTTPS
+updateurlipv4="https://"$hostname":"$apitoken$updateurlbaseipv4$currentipv4
+updateurlipv6="https://"$hostname":"$apitoken$updateurlbaseipv6$currentipv6
 
 if [ -f "$lastipfile" ]; then
 
 	if [ "$currentipv4" == "$lastipv4" ]; then
-		echo "IPv4 ist gleich"
+		echo "Local IPv4 ist gleich"
+		if [ "$currentipv4" == "$dnsipv4" ]; then
+			echo "DNS IPv4 ist gleich"
+		else
+			sed -i "s/$lastipv4/$currentipv4/g" $lastipfile
+			get -q -O --no-check-certificate "${updateurlipv4}"
+		fi
 	else
-		sed -i "s/$lastipv4/$currentipv4/g" /etc/lastip.txt
+		sed -i "s/$lastipv4/$currentipv4/g" $lastipfile
 		wget -q -O --no-check-certificate "${updateurlipv4}"
 	fi
 
 	if [ "$currentipv6" == "$lastipv6" ]; then
-		echo "IPv6 ist gleich"
+		echo "Local IPv6 ist gleich"
+                if [ "$currentipv6" == "$dnsipv6" ]; then
+                        echo "DNS IPv6 ist gleich"
+                else
+                        sed -i "s/$lastipv6/$currentipv6/g" $lastipfile
+                        get -q -O --no-check-certificate "${updateurlipv6}"
+                fi
 	else
-		sed -i "s/$lastipv6/$currentipv6/g" /etc/lastip.txt
+		sed -i "s/$lastipv6/$currentipv6/g" $lastipfile
 		wget -q -O --no-check-certificate "${updateurlipv6}"
 	fi
 
